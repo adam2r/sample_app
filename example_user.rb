3611542493f8e0863b16115e0949254633699d50
Adam Robinson <acirobinson@gmail.com>
2017-01-06 17:13:33 +0000

    Finsih layout an d routes

---
+++ example_user.rb
@@ -0,0 +1,14 @@
+class User
+	
+	attr_accessor :name, :email
+
+	def initialize(attributes = {})
+		@name = attributes[:name]
+		@email = attributes[:email]
+	end
+
+	def formatted email
+		"#{@name} <#{@email}>"
+	end
+
+end
