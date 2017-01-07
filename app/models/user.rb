d2d9cf14b60585bcd018106365c706e31af39f9e
Adam Robinson <acirobinson@gmail.com>
2017-01-07 09:35:39 +0000

    Finish signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-	before_Save {self.email = email.downcase }
-	valdiates :name, presence: true, length: { maximum: 30 }
+	before_save {self.email = email.downcase }
+	validates :name, presence: true, length: { maximum: 30 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 100 }, 
 					format: { with: VALID_EMAIL_REGEX},
